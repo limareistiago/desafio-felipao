6a25da631efd273870e92b7a67237ef56126f01b
limareistiago <limareistiago@gmail.com>
2025-12-07 08:33:16 -0300

    commit final

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 let nome = "Etab-moc ";
-let xp = "XP"; 
-let exp = 900;
+let xp = 7030;
+let nivel;
 
 
-if (exp <= 1000) {
-    console.log("O Herói de nome " + nome  + "está no nivél " + xp + " Ferro");
+if (xp < 1000) {
+    nivel = "ferro";
+}else if (xp >= 1001 && xp <= 2000) {
+        nivel = "Bronze";
+} else if (xp >= 2001 && xp <= 5000) {
+        nivel = "Prata";
+} else if (xp >= 5001 && xp <= 7000) {
+        nivel = "Ouro";
+} else if (xp >= 7001 && xp <= 8000) {
+        nivel = "Platina";
+} else if (xp >= 8001 && xp <= 9000) {
+        nivel = "Ascendente";
+} else if (xp >= 9001 && xp <= 10000) {
+        nivel = "Imortal";
+} else { // Cobre XP maior ou igual a 10001
+        nivel = "Radiante";
 }
+
+console.log("O Herói de nome " + nome + "está no nível de " + nivel);
